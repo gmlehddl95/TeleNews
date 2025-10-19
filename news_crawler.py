822355785823ecf102b92b1e9f15e4b1d32f587b
gmlehddl95 <gmlehddl95@gmail.com>
2025-10-19 21:37:42 +0900

    언론사 매핑 추가 업데이트

--- news_crawler.py
+++ news_crawler.py
@@ -202,7 +202,10 @@ class NaverNewsCrawler:
                                 'kado.net': '강원도민일보', 'pennmike.com': '펜엔마이크',
                                 'etoday.co.kr': '이투데이', 'newsprime.co.kr': '프라임경제',                                 'nongmin.com': '농민신문', 'kookje.co.kr': '국제신문', 
                                 'newscj.com': '천지일보', 'pointdaily.co.kr': '포인트데일리', 
-                                'daily.hankooki.com': '데일리한국', 'news.einfomax.co.kr': '연합인포맥스'
+                                'daily.hankooki.com': '데일리한국', 'news.einfomax.co.kr': '연합인포맥스',
+                                'view.asiae.co.kr' : "아시아경제", 'ohmynews.com' : '오마이뉴스',
+                                'news.tf.co.kr': '더팩트', 'ichannela.com': '채널A',
+                                'pressian.com': '프레시안'
                             
                             }
                             
